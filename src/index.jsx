8555ed3dee2fb4020c9e8f1a65f775047092e0ad
Mark Saunders <saunders99999@gmail.com>
2019-10-07 19:53:58 -0400

    Initial Commit

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+export default function render(data) {
+    var header = data.conditional ? <div /> : null;
+    var collection = data.items.map((item) => {
+        return <li key={item.id} class={item.className}>{item.name}</li>;
+    });
+ 
+    return <div id="container">
+        {header}
+        <ul>{collection}</ul>
+        <p {...data.props}>Some features</p>
+    </div>;
+}
